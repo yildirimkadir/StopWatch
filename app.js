763dea6785d183f3a9951bd5199f295f3b40ed6a
yildirimkadir <yildirimkad34@gmail.com>
2022-07-18 23:57:39 +0200

    "updated"

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+const button = document.querySelector("#handler");
+button.addEventListener("click");
 
 const startWatch = () => {
     seconds++;
@@ -14,6 +16,50 @@ const startWatch = () => {
             hours++;
         }
     }
-    
-    document.getElementById("display").innerHTML = `${hours} : ${minutes} : ${seconds}`;
+
+    if(seconds < 10 ) {
+		displaySeconds = '0' + seconds.toString();
+	}
+	else {
+		displaySeconds = seconds;
+	}
+
+	if(minutes < 10 ) {
+		displayMinutes = '0' + minutes.toString();
+	}
+	else {
+		displayMinutes = minutes;
+	}
+
+	if(hours < 10 ) {
+		displayHours = '0' + hours.toString();
+	} else {
+		displayHours = hours;
+	}
+
+	document.getElementById('display').innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 };
+
+function startStop() {
+	if(status == 'Stopped') {
+		interval = window.setInterval(StartWatch,1000);
+		document.getElementById('handler').innerHTML = 'Stop';
+		status = 'Started';
+	}
+	else if(status == 'Started') {
+		window.clearInterval(interval);
+		document.getElementById('handler').innerHTML = 'Start';
+		status = 'Stopped';
+	}
+}
+
+function Reset()
+{
+	seconds = 0;
+	hours = 0;
+	minutes = 0;
+	window.clearInterval(interval);
+	document.getElementById('display').innerHTML = '00:00:00';
+	document.getElementById('handler').innerHTML = 'Start';
+	status = 'Stopped';
+}
